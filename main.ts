16f735da0b44d4f105213b32f81ed8cf47cc4dfc
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-28 17:45:01 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,14 @@ basic.pause(1000)
 cuteBot.forward()
 basic.forever(function () {
     vzdalenost = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    if (vzdalenost > 2 && vzdalenost < 15) {
+    if (vzdalenost >= 0 && vzdalenost < 15) {
         cuteBot.motors(0, 0)
         for (let index = 0; index < 4; index++) {
             music.playTone(880, music.beat(BeatFraction.Quarter))
         }
         basic.pause(2000)
         cuteBot.motors(randint(-50, -100), 0)
+        basic.pause(500)
     } else {
         cuteBot.forward()
     }
