216d82955374475dc4e2527de3f6c38535e015a2
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-28 17:37:10 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+let vzdalenost = 0
+basic.showIcon(IconNames.Snake)
+basic.pause(1000)
+cuteBot.forward()
+basic.forever(function () {
+    vzdalenost = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (vzdalenost > 2 && vzdalenost < 15) {
+        cuteBot.motors(0, 0)
+        for (let index = 0; index < 4; index++) {
+            music.playTone(880, music.beat(BeatFraction.Quarter))
+        }
+        basic.pause(2000)
+        cuteBot.motors(randint(-50, -100), 0)
+    } else {
+        cuteBot.forward()
+    }
+})
